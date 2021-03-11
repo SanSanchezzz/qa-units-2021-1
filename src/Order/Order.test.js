b96416e5ee7f360918baddaa4b686ee123c98635
SanSanchezzz <13lex04@gmail.com>
2021-03-11 21:42:30 +0300

    Выполнить задание

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -1,8 +1,65 @@
 import React from 'react'
+import {shallow, configure} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import {fakeOrders} from "../data/fakeOrders";
+import Order from "./Order";
+
+jest.mock('../utils/getDate');
+import {getDate} from '../utils/getDate';
+
+configure({adapter: new Adapter()});
 
 describe('Order.js', () => {
-  it('some test', () => {
+  beforeEach(() => {
+    getDate.mockReturnValue('11 января');
+  });
 
+  afterEach( () => {
+    getDate.mockClear();
+  });
+
+  it('order is null', () => {
+    const order = fakeOrders[0];
+
+    const wrapper = shallow(<Order
+        order = {null}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+    expect(getDate).toHaveBeenCalledTimes(0);
+
+  });
+
+  it('shop and date are null', () => {
+    const wrapper = shallow(<Order
+        order = {{shop: null, date: null}}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+    expect(getDate).toHaveBeenCalledTimes(0);
+  });
+
+  it('getDate has been called', () => {
+    const order = fakeOrders[0];
+
+    const wrapper = shallow(<Order
+        order = {order}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+    expect(getDate).toHaveBeenCalledTimes(1);
+  });
+
+  it('getDate has been called', () => {
+    const order = fakeOrders[0];
+
+    const wrapper = shallow(<Order
+        order = {{shop: 'shop', date: 'date', items: null}}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+    expect(getDate).toHaveBeenCalledTimes(1);
   });
 });
 
--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, getSortFunction, sortByDate, sortOrders} from './sortOrders';
+import {sortTypes} from "./sortOrders";
+import {fakeOrders} from "../data/fakeOrders";
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
 		const result = sortByItemCount(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('order are not object', () => {
+		const result = sortByItemCount(null, 1);
 		expect(result).toEqual(0);
 	});
 
@@ -20,5 +27,152 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('the first order has more items than the second', () => {
+		const order1 = {
+			items: ['1', '2', '3'],
+		};
+
+		const order2 = {
+			items: ['1', '2'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('the second order has more items than the first', () => {
+		const order1 = {
+			items: ['1', '2'],
+		};
+
+		const order2 = {
+			items: ['1', '2', '3'],
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+
+	it('items are null', () => {
+		const order1 = {
+			items: null,
+		};
+
+		const order2 = {
+			items: null,
+		};
+
+		const result = sortByItemCount(order1, order2);
+
+		expect(result).toBe(0);
+	});
 });
 
+describe('sortByDate function', () => {
+	it('orders are null', () => {
+		const result = sortByDate(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('order are not object', () => {
+		const result = sortByDate(null, 1);
+		expect(result).toEqual(0);
+	});
+
+	it('dates are null', () => {
+		const order1 = {
+			date: null,
+		};
+
+		const order2 = {
+			date: null,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+
+	it('the first order was earlier than the second', () => {
+		const order1 = {
+			date: 1544356800000,
+		};
+
+		const order2 = {
+			date: 1552481120000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(1);
+	});
+
+	it('the second order was earlier than the first', () => {
+		const order1 = {
+			date: 1552481120000,
+		};
+
+		const order2 = {
+			date: 1544356800000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(-1);
+	});
+
+	it('same date', () => {
+		const order1 = {
+			date: 1552481120000,
+		};
+
+		const order2 = {
+			date: 1552481120000,
+		};
+
+		const result = sortByDate(order1, order2);
+
+		expect(result).toBe(0);
+	});
+});
+
+describe('getSortFunction function', () => {
+	it('sortType by Date', () => {
+		const result = getSortFunction(sortTypes.DATE);
+		expect(result).toEqual(sortByDate);
+	});
+
+	it('sortType by Count', () => {
+		const result = getSortFunction(sortTypes.COUNT);
+		expect(result).toEqual(sortByItemCount);
+	});
+
+	it('sortType is null', () => {
+		const result = getSortFunction(null);
+		expect(result).toBeUndefined();
+	});
+});
+
+describe('sortOrders function', () => {
+	it('callback has been called', () => {
+		const callback = jest.fn();
+		sortOrders(fakeOrders, callback);
+		expect(callback).toHaveBeenCalled();
+	});
+
+	it('orders are null', () => {
+		const callback = jest.fn();
+		sortOrders(null, callback);
+		expect(callback).toHaveBeenCalledTimes(0);
+	});
+
+	it('orders are null', () => {
+
+		const orders = fakeOrders;
+		sortOrders(orders, null);
+		expect(orders).toStrictEqual(fakeOrders);
+	});
+});
