a212aaf1ae73a3c7987aeeb17c9bb1428775f594
SanSanchezzz <13lex04@gmail.com>
2021-03-12 17:28:33 +0300

    Декомпозировать test case-ы для Order.js

--- src/Order/Order.test.js
+++ src/Order/Order.test.js
@@ -35,16 +35,33 @@ describe('Order.js', () => {
     expect(getDate).toHaveBeenCalledTimes(0);
   });
 
-  it('shop and date are null', () => {
+  it('render with shop and date are null', () => {
     const wrapper = shallow(<Order
         order = {{shop: null, date: null}}
     />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('getDate not called with shop and date are null', () => {
+    shallow(<Order
+        order = {{shop: null, date: null}}
+    />);
+
     expect(getDate).toHaveBeenCalledTimes(0);
   });
 
   it('getDate has been called', () => {
+    const order = fakeOrders[0];
+
+    shallow(<Order
+        order = {order}
+    />);
+
+    expect(getDate).toHaveBeenCalledTimes(1);
+  });
+
+  it('render with some fake data', () => {
     const order = fakeOrders[0];
 
     const wrapper = shallow(<Order
@@ -52,16 +69,22 @@ describe('Order.js', () => {
     />);
 
     expect(wrapper).toMatchSnapshot();
-    expect(getDate).toHaveBeenCalledTimes(1);
   });
 
   it('getDate has been called without items', () => {
+    shallow(<Order
+        order = {{shop: 'shop', date: 'date', items: null}}
+    />);
+
+    expect(getDate).toHaveBeenCalledTimes(1);
+  });
+
+  it('render with items null', () => {
     const wrapper = shallow(<Order
         order = {{shop: 'shop', date: 'date', items: null}}
     />);
 
     expect(wrapper).toMatchSnapshot();
-    expect(getDate).toHaveBeenCalledTimes(1);
   });
 });
 
